3d6ebec7ab2fe00a268dea20780b79d87a5e9521
GSmithApps <14.gsmith.14@gmail.com>
2023-03-05 09:35:05 -0600

    matched scratch.jl

--- scratch.jl
+++ scratch.jl
@@ -1,60 +1,62 @@
 using Plots
+
+
+# try to get an initial neural network
+# to learn the exclusive or function
+
+# randomly generate some domain points in 2D as Float32
+domainPoints = rand(Float32, 2, 1000)
+
+
+# classify them with an exclusive or
+labels = [(col[1] < .5) ⊻ (col[2] < .5) for col in eachcol(domainPoints)]
+
+
+# try to plot the points.
+# this will break because the labels are not numbers
+# we'll convert them to numbers in the next step
+scatter(domainPoints[1,:], domainPoints[2,:], color=labels)
+
+# map the labels to 1 and 0
+labels = map(x -> x ? 1 : 0, labels)
+
+# plot the points.
+scatter(domainPoints[1,:], domainPoints[2,:], color=labels)
+
+
+# change the color of the points
+# to be a grayscale value
+scatter(domainPoints[1,:], domainPoints[2,:], markercolor=Gray.(labels))
+
+data = [(domainPoints[:, i], labels[i]) for i in eachindex(labels)]
+
+# start the learning
 using Flux
 
+# train the model
+loss(x, y) = Flux.mse(model(x), y)
 
 # define a simple 2 layer neural network
 model = Chain(
-  Dense(2 => 3, tanh),   # activation function inside layer
-  BatchNorm(3),
-  Dense(3 => 2),
-softmax)
+  Dense(2, 16, σ),
+  Dense(16, 1, σ),
+)
 
+train_loss = []
 
-# train the model
-loss(x, y) = Flux.Losses.crossentropy(model(x), y)
+lr = 0.1
 
-opt = ADAM(0.1)
-
-# create a function with a body
-
-function generateAndReturnDomain()
-  """
-  Generate a domain of points and return it
-  """
-
-  range = LinRange(0,1,75)
-
-  domainPoints = zeros(2,length(range) ^ 2)
-
-  i = 1
-
-  for x in range
-    for y in range
-      domainPoints[1,i] = x
-      domainPoints[2,i] = y
-      i = i + 1
-    end
-  end
-
-  return domainPoints
-
+for i in 1:100
+  opt = ADAM(lr)
+  Flux.train!(loss, Flux.params(model), data, opt)
+  push!(train_loss, loss(domainPoints, reshape(labels,1,:)))
+  lr = lr * .98
 end
 
-domainPoints = generateAndReturnDomain()
-
-# classify them with an exclusive or
-labels = [(col[1] < .5) ⊻ (col[2] < .5) ? 1 : 0 for col in eachcol(domainPoints)]
-
-labelsOneHot = Flux.onehotbatch(labels, [0,1])
+# plot the loss values over time
+plot(train_loss,ylims = (0,maximum(train_loss)*1.1), xlabel="Iteration", ylabel="Loss", legend=false)
 
 
-data = (domainPoints, labels)
+ŷ = model(domainPoints)
 
-# load the data
-loadedData = Flux.DataLoader(data, batchsize=10)
-
-first(loadedData)[2]
-
-for i in loadedData[1:10]
-  
-end
+scatter(domainPoints[1,:], domainPoints[2,:], markercolor=reshape(Int.(round.(ŷ)),:))
